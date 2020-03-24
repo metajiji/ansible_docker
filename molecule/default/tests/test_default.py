ba3712b1530f675656cf9e7a21183fe6e1f2dafc
Denis Kadyshev <metajiji@gmail.com>
2020-03-24 16:05:23 +0700

    Add static installation

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,3 +10,12 @@ def test_dockerd_running_and_enabled(host):
     service = host.service('docker')
     assert service.is_enabled
     assert service.is_running
+
+# TODO: why not listen?
+# def test_dockerd_is_listen(host):
+#     socket = host.socket('unix:///run/docker.sock')
+#     assert socket.is_listening
+
+# def test_containerd_is_listen(host):
+#     socket = host.socket('unix:///run/containerd/containerd.sock')
+#     assert socket.is_listening
